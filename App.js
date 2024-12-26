a8b1ed669709f16796737dd2a0b6033fccb5ae2f
RadwaAhmed4869 <48102202+RadwaAhmed4869@users.noreply.github.com>
2024-12-26 06:02:05 +0200

    Initial commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View, Button } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View></View>
+      <View></View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  
+});
